ea8ca929967a0b74be0a9eaecefb5c81a5c65707
jason gilbertson <jagilber@microsoft.com>
2022-06-02 10:23:00 -0400

    v1 test

--- build.ps1
+++ build.ps1
@@ -1,11 +1,17 @@
 # https://docs.microsoft.com/en-us/azure/devops/extend/develop/add-build-task?view=azure-devops
 
 param(
-    $vstsAccount = 'jagilber@microsoft.com'
+    $localEnv = 'secrets.ps1',
+    $root = $pwd
 )
-$root = $pwd
+
 if($PSScriptRoot) {
     $root = $PSScriptRoot
+}
+
+if((test-path $root\$localEnv)) {
+    . $root\$localEnv
+    write-host "using vstsAccount:$vstsAccount"
 }
 
 write-host "cd $root\buildandreleasetask"
@@ -14,16 +20,16 @@ write-host "tsc"
 tsc
 write-host "cd $root"
 cd $root
-write-host "tfx extension create --manifest-globs vss-extension.json"
-tfx extension create --manifest-globs vss-extension.json
+write-host "tfx extension create --manifest-globs vss-extension.json --rev-version"
+tfx extension create --manifest-globs vss-extension.json --rev-version
 
 $vsix = (resolve-path *.vsix).Path
-
-write-host "tfx extension publish --manifest-globs vss-extension.json --vsix $vsix --share-with $vstsAccount"
-tfx extension publish --manifest-globs vss-extension.json --vsix $vsix --share-with $vstsAccount
+# https://docs.microsoft.com/en-us/azure/devops/extend/publish/command-line?view=azure-devops
+# tfx publish requires a pat with access to 'all organizations' which is not allowed on ms internal subs :|
+write-host "tfx extension publish --manifest-globs vss-extension.json --vsix $vsix --share-with $vstsAccount --auth-type pat -t $vstsPat"
+#tfx extension publish --manifest-globs vss-extension.json --vsix $vsix --share-with $vstsAccount --auth-type pat -t $vstsPat
 
 # https://marketplace.visualstudio.com/manage/createpublisher?managePageRedirect=true
 # https://marketplace.visualstudio.com/manage/publishers/jagilber
 
 #[System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("C:\path\to\certificate.pfx"))
-[System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("C:\temp\sfjagilber-sfjagilber-20220525.pfx"))
